3116b2f755b86add512d5af94fd21b5f1b923311
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-13 16:44:07 -0600

    changed config back

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,5 @@ module APILivePolling
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
