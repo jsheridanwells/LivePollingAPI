48039fff84442f56eebc78e7426384b4c749a7c5
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-13 14:39:50 -0600

    created current_slides migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213152201) do
+ActiveRecord::Schema.define(version: 20171213203647) do
 
   create_table "items", force: :cascade do |t|
     t.string "content"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171213152201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "broadcasting", default: false
+    t.integer "current_slide", default: 0
     t.index ["user_id"], name: "index_presentations_on_user_id"
   end
 
