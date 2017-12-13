10dcecc9b966a0ffb233a90089435aef7441e66f
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-13 16:55:52 -0600

    moved json_web_token.rb to models, changed config.autoload_paths << Rails.root.join(lib)

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,6 @@ module APILivePolling
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('app/models')
   end
 end
