a0b72a564f3f759dec5c35a0b982155df92482c9
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-14 19:54:48 -0600

    refactored count method

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -6,6 +6,7 @@ class ResponsesController < ApplicationController
     response = Response.new(response_params)
     if response.save
       bundle_responses(@presentation.polls)
+      # count_responses(@presentation.polls)
       ActionCable.server.broadcast "response_channel#{params[:id]}", { data: @response_arr }
     else
       render json: response.errors, status: :unprocessable_entity
@@ -21,15 +22,30 @@ class ResponsesController < ApplicationController
       @presentation = Presentation.find(params[:id])
     end
 
+
+    # this might get replaced by count_responses
     def bundle_responses(poll_arr)
       @response_arr = Array.new
       poll_arr.each do |poll|
         items = Item.where(poll_id: poll.id)
         items.each do |item|
-          responses = Response.where(item_id: item.id)
-          @response_arr.push(responses) if responses.length != 0
+          responses = Response.where(item_id: item.id).count
+          @response_arr.push(responses) if responses > 0
         end
       end
     end
+
+    # def count_responses(poll_arr)
+    #   @response_arr = Array.new
+    #   poll_arr.each do |poll|
+    #     items = Item.where(poll_id: poll.id)
+    #     items.each do |item|
+    #       response_count = Response.where(item_id: 1).count
+    #       puts "$$$$$$$$$$$$$$response_count: #{response_count}"
+    #       @response_arr.push(response)
+    #     end
+    #   end
+
+    # end
 
 end
