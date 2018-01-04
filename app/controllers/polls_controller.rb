1f6050b2cf1b935234542ea93fab8906d7b23bcf
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-01-04 14:15:46 -0600

    edit and delete tested and working

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -27,6 +27,13 @@ class PollsController < ApplicationController
 
   # PATCH/PUT /polls/1
   def update
+    @items = Item.where(poll_id: @poll.id)
+    puts "poll params: #{poll_params}"
+    # @items.each do |item|
+    #   poll_params[:poll].item_attributes.each do |edited_item|
+    #     item.destroy unless item.id = edited_item.id
+    #   end
+    # end
     if @poll.update(poll_params)
       render json: @poll
     else
