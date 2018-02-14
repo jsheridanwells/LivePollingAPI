91ca0fff2423b0aefd528b61237a242df11e7b13
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-14 12:41:09 -0600

    some refacotring and cleaning up

--- app/channels/response_channel.rb
+++ app/channels/response_channel.rb
@@ -2,8 +2,4 @@ class ResponseChannel < ApplicationCable::Channel
   def subscribed
     stream_from "response_channel#{params[:presentation_id]}"
   end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
 end
